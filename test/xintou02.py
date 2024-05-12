ce24b9e477aae256e5d5de397ad75ac8ce800216
lk <asliukuias@126.com>
2024-05-12 13:44:30 +0800

    当无金额字段时，取消对比例金额输出

--- test/xintou02.py
+++ test/xintou02.py
@@ -329,8 +329,8 @@ for sheet in sheets_data:
             sqlcp4 += sqlcp4_1 + sqlcp4_2 + sqlcp4_3
             if len1 + len2 + len3 > 1:
                 sqlcp4 += ")\n"
-
-            df.loc[4, df.columns[col_num_ct_sql]] = sqlcp4
+            if sqlcp4_cnt> 0:
+                df.loc[4, df.columns[col_num_ct_sql]] = sqlcp4
             df.loc[5, df.columns[col_num_ct_sql]] = f"select count(1) as toucnt from {mainList[0].table_name}"
             df.loc[6, df.columns[col_num_ct_sql]] = f"select 0 as toucnt from dual"
             df.loc[7, df.columns[col_num_ct_sql]] = sqlcp7
